5741d196a906d1bbe881ad1e918345c6e59d4dad
Melissa Holt <melissa.holt21@gmail.com>
2019-12-09 01:31:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -9,5 +9,33 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create(song)
+    Song.all.include?(song)
+    song
+  end
+  
+  def self.new_by_name
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name
+  end
+  
+  def self.alphabetical
+  end
+  
+  def self.new_from_filename
+  end
+  
+  def self.create_from_filename
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
 end
