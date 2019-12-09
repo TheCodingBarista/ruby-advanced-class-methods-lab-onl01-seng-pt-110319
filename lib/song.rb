fd6943803c6607b47946f026d0b32a8d67813928
Melissa Holt <melissa.holt21@gmail.com>
2019-12-09 02:09:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,8 +40,9 @@ class Song
     self.all.sort_by { |song| song.name }
   end
   
-  def self.new_from_filename()
+  def self.new_from_filename(filename)
     song = self.new
+    split_filename = filename.delete_at
     song.name = name
     song.artist_name = artist_name
     song
