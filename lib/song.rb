732007aa322376c44ebafccd4b1524682760d6f5
Melissa Holt <melissa.holt21@gmail.com>
2019-12-09 01:38:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,11 @@ class Song
    song
   end
   
-  def self.new_by_name
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
   end
   
   def self.find_by_name(name)
